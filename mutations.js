3dde82dc6fb57d218c566db2f52cf6effd590316
Daniel Cleveland <dpcleveland@gmail.com>
2016-02-14 14:51:13 -0600

    Mutations bonfire

--- mutations.js
+++ mutations.js
@@ -5,7 +5,25 @@ in the second element of the array.
 */
 
 function mutation(arr) {
-    return arr;
+    // Use the toLowerCase method to change the elements to lowercase
+    // This is because the indexOf method is case-sensitive
+    var firstElement = arr[0].toLowerCase();
+    var secondElement = arr[1].toLowerCase();
+
+    // Loop through the indexOf method
+    // Until you get to the end of teh second element
+    for (i = 0; i < secondElement.length; i++) {
+        // Use indexOf to see if there are any characters that don't match up
+        if (firstElement.indexOf(secondElement[i]) === -1) {
+            return false;
+        }
+    }
+    // If all the characters matched up
+    // Break out of the for loop and return true
+    return true;
 }
+
+
+
 
 mutation(["hello", "hey"]);
