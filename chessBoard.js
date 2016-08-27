2ff1ad34f7933d5eb10cbe28b393edf06e2a2faf
Daniel Cleveland <dpcleveland@gmail.com>
2016-08-27 18:38:38 -0500

    Chess Board exercise

---
+++ chessBoard.js
@@ -0,0 +1,19 @@
+// My solution
+var board = "";
+var size = 10;
+
+for (var i = 1; i <= size; i++) {
+    for (var j = 1; j <= size; j++) {
+        if ((i + j) % 2 == 0) {
+            board += " ";
+        }
+        else {
+            board += "#";
+        }
+    }
+    board += "\n";
+}
+
+console.log(board);
+
+// Eloquent JavaScript solution is pretty much the same
