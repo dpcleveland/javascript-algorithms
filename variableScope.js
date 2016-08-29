a81249d1d9432ac383e65870f12c3a2f741ca2c9
Daniel Cleveland <dpcleveland@gmail.com>
2016-08-28 21:05:00 -0500

    Variable scope exercise

---
+++ variableScope.js
@@ -0,0 +1,15 @@
+var x = "outside";
+
+var f1 = function() {
+    var x = "inside f1";
+};
+f1();
+console.log(x);
+// -> outside
+
+var f2 = function() {
+    x = "inside f2";
+}
+f2();
+console.log(x);
+// -> inside f2
