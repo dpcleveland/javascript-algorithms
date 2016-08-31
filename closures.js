11247734a22f685b86a80b999af05b60a4eb76d3
Daniel Cleveland <dpcleveland@gmail.com>
2016-08-31 18:59:09 -0500

    Static vs dynamic scope

--- closures.js
+++ closures.js
@@ -109,3 +109,23 @@ console.log(moar(15));
 // -> -80
 
 /////////////////////////
+
+var x = 10;
+
+function fooScope() {
+    var y = x + 5;
+    return y;
+}
+
+function barScope() {
+    var x = 2;
+    return fooScope();
+}
+
+function main() {
+    fooScope(); // Static scope: 15; Dynamic scope: 15
+    barScope(); // Static scope: 15; Dynamic scope: 7
+    return 0;
+}
+
+/////////////////////////
