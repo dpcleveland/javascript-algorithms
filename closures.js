e4aef8434ed466935c631c56d4eae9da1490a77f
Daniel Cleveland <dpcleveland@gmail.com>
2016-08-31 18:37:39 -0500

    Multiple level closure

--- closures.js
+++ closures.js
@@ -70,3 +70,20 @@ sayHelloClosure();
 // -> Hello, world!
 
 /////////////////////////
+
+function outer() {
+    var middle = function() {
+        var inner = function() {
+            console.log(hello);
+        }
+        return inner;
+    }
+    var hello = "Hello, world! This is a multi-closure.";
+    return middle;
+}
+var middleClosure = outer();
+var innerClosure = middleClosure();
+innerClosure();
+// -> Hello, world!
+
+/////////////////////////
