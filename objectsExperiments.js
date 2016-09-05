4a2038fadbd7b37727baa30409d5dfbc8f1823ad
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-05 13:48:59 -0500

    Mutability

--- objectsExperiments.js
+++ objectsExperiments.js
@@ -40,3 +40,18 @@ var journal = [
     }
 ];
 console.log(journal[0].events[2]);
+
+var object1 = {value: 10};
+var object2 = object1;
+var object3 = {value: 10};
+
+console.log(object1 == object2);
+// -> true
+console.log(object1 == object3);
+// -> false
+
+object1.value = 15;
+console.log(object2.value);
+// -> 15
+console.log(object3.value);
+// -> 10
