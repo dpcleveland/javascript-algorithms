628970fac4664054fef3eeb84ff54b791b2194fb
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-04 19:22:55 -0500

    Changed function naming

--- minimum.js
+++ minimum.js
@@ -1,7 +1,7 @@
 // Example functionality
 // console.log(Math.min(1, 1));
 
-function minimum(val1, val2) {
+function min(val1, val2) {
     if (val1 < val2) {
         return val1;
     }
@@ -9,9 +9,9 @@ function minimum(val1, val2) {
         return val2;
     }
 }
-console.log(minimum(0, 10));
+console.log(min(0, 10));
 // -> 0
-console.log(minimum(0, -10));
+console.log(min(0, -10));
 // -> -10
-console.log(minimum(1, 1));
+console.log(min(1, 1));
 // -> 1
