22b660dedc869bfffabb8dc054bb9a2aadae2940
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-05 12:57:19 -0500

    More tests

--- propertiesExperiments.js
+++ propertiesExperiments.js
@@ -5,3 +5,9 @@ console.log(firstVal);
 
 var myVarLength = myVar.length;
 console.log(myVarLength);
+
+var myVarLengthBrackets = myVar["length"];
+console.log(myVarLengthBrackets);
+
+var fifthVal = myVar[3+2];
+console.log(fifthVal);
