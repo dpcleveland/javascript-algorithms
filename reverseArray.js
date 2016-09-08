cb050fb97c9f02fa544ad1178e9fbad0cdc892db
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-07 19:53:01 -0500

    Added EJ answer

--- reverseArray.js
+++ reverseArray.js
@@ -8,3 +8,15 @@ function reverseArray(array) {
 
 console.log(reverseArray(["A", "B", "C"]));
 // -> ["C", "B", "A"];
+
+/*
+// Eloquent JavaScript's answer
+
+function reverseArray(array) {
+    var output = [];
+    for (var i = array.length - 1; i >= 0; i--) {
+        output.push(array[i]);
+    }
+    return output;
+}
+*/
